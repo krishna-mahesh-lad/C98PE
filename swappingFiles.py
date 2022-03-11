ac455f00f7c8f3ea22207b7f76ba9953ecab550b
Euphoria <68733354+krishna-mahesh-lad@users.noreply.github.com>
2022-03-11 14:52:55 +0530

    Add files via upload

---
+++ swappingFiles.py
@@ -0,0 +1,22 @@
+file1 =  input('Insert File 1 Name: ')
+file2 =  input('Insert File 2 Name: ')
+#r = ''.read
+r = open(file1, file2, 'r')
+
+for l in r:
+  print(l)
+
+def swapFileData():
+    with open(file1, 'r') as a:
+        data_a = a.read()
+
+    with open(file2, 'r') as b:
+        data_b = b.read()
+
+    with open(file1, 'w') as a:
+        a.write(data_b)
+
+    with open(file2, 'w') as b:
+        b.write(data_a)
+
+swapFileData()
